fb39be587fc6478c2dfa093717654fed3e21d2d0
Etienne Visinoni <etienne.visinoni@gmail.com>
2024-03-21 22:01:36 +0100

    Add save State for color variable
    Change enging lines from LF to CRLF for MenuUIHandler.cs
    
    + save data class
    + SaveColor method
    +LoadColor method

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -14,7 +14,7 @@ public class MenuUIHandler : MonoBehaviour
 
     public void NewColorSelected(Color color)
     {
-        // add code here to handle when a color is selected
+        MainManager.Instance.TeamColor = color;
     }
 
     private void Start()
@@ -22,6 +22,8 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void StartGame()
@@ -31,6 +33,7 @@ public class MenuUIHandler : MonoBehaviour
 
     public void QuitGame()
     {
+        MainManager.Instance.SaveColor();
 
         Debug.Log("quitgame fired");
 
@@ -39,9 +42,17 @@ public class MenuUIHandler : MonoBehaviour
 #else
         Application.Quit(); // original code to quit Unity player
 #endif
-
-
-
-
     }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }
+
 }
